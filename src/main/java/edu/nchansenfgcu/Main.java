ce9c1cf981fbb102a8c6945967b690a92e061ef3
anagalacticRuby <841b2dRuby>
2020-02-05 21:33:22 -0500

    Initial commit

---
+++ src/main/java/edu/nchansenfgcu/Main.java
@@ -0,0 +1,14 @@
+package edu.nchansenfgcu;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.IOException;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException, CsvValidationException {
+   CsvParser csvP = new CsvParser("src/Data/SEOExample-1.csv");
+  csvP.printCsv();
+  }
+}
